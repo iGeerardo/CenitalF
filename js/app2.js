9801f21c64373d7c1132fde34560bcc3f2fae420
iGeerardo <62261769+iGeerardo@users.noreply.github.com>
2022-03-08 13:26:03 -0600

    Add files via upload

---
+++ js/app2.js
@@ -0,0 +1,6 @@
+const   btnMenu2 = document.querySelector('#menu2'),
+        menuContent2 = document.querySelector('.menu-content2');
+
+btnMenu2.addEventListener('click', () =>{
+    menuContent2.classList.toggle('menu-active2');
+});
